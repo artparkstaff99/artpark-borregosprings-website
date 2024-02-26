d94d8070712aaf4eafbe73b60b725ffbad1aaf19
pkellner <peter@peterkellner.net>
2024-02-26 10:42:18 -0800

    flatten

--- app/keystatic/layout.tsx
+++
@@ -1,5 +0,0 @@
-import KeystaticApp from './keystatic'
-
-export default function RootLayout() {
-  return <KeystaticApp />
-}
--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: false,
-  images: {
-    domains: ['tstr09nnhnz3.keystatic.net'],
-  },
-}
