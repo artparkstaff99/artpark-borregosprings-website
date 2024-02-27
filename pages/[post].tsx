ea966f88ba59140aa8c7e60f1e39456938e4aacc
pkellner <peter@peterkellner.net>
2024-02-27 05:20:20 -0800

    x

--- pages/[post].tsx
+++ pages/[post].tsx
@@ -16,6 +16,10 @@ import TweetEmbed from "../components/TweetEmbed";
 import LoopingVideo from "../components/LoopingVideo";
 import Testimonial from "../components/Testimonial";
 import AvatarList from "../components/AvatarList";
+import Header from "../components/Header";
+import React from "react";
+import Footer from "../components/Footer";
+import { getHomeData } from "../utils/get-static-page-utils";
 
 export async function getStaticPaths() {
   const reader = createReader("", config);
@@ -47,8 +51,10 @@ export const getStaticProps = async ({ params }: GetStaticPropsContext) => {
     post.authors.map(async (authorSlug) => {
       const author = await reader.collections.authors.read(authorSlug || "");
       return { ...author, slug: authorSlug };
-    })
+    }),
   );
+
+  const [home] = await Promise.all([getHomeData()]);
 
   return {
     props: {
@@ -57,6 +63,7 @@ export const getStaticProps = async ({ params }: GetStaticPropsContext) => {
         slug,
       },
       authors: authorsData,
+      home,
     },
   };
 };
@@ -69,112 +76,121 @@ export type AuthorProps = InferGetStaticPropsType<
 type TheLot = {
   post: PostProps;
   authors: AuthorProps;
+  home: any;
 };
 
-export default function Post({ post, authors }: TheLot) {
+export default function Post({ post, authors, home}: TheLot) {
   const names = authors.reduce(
     (acc: string[], author) =>
       "name" in author ? [...acc, author.name as string] : acc,
-    []
+    [],
   );
   const formattedNames = new Intl.ListFormat("en-AU")
     .format(names)
     .replace("and", "&");
   return (
-    <div className="max-w-4xl mx-auto px-4 md:px-10">
-      <Seo
-        title={post.title}
-        description={post?.summary}
-        imagePath={
-          post.coverImage
-            ? `/images/posts/${post.slug}/${post.coverImage}`
-            : "/images/seo-image.png"
-        }
-      />
-      <div className="flex gap-3 items-center flex-wrap">
-        {authors && <AvatarList authors={authors} />}
-        <p className="font-semibold text-gray-900">{formattedNames}</p>
-      </div>
-
-      <div className="mt-4 flex justify-between">
-        <span className="flex gap-1 text-gray-700">
-          {post.publishedDate && (
-            <p className="">
-              {dateFormatter(post.publishedDate, "do MMM yyyy")}
-            </p>
-          )}
-          {post.wordCount && post.wordCount !== 0 ? (
-            <p className="">· {readTime(post.wordCount)}</p>
-          ) : null}
-        </span>
-      </div>
-
-      <div className="mt-8 prose max-w-none">
-        <h1 className="mt-4">{post.title}</h1>
-        <p className="text-lg">{post.summary}</p>
-        {post.coverImage && (
-          <div className="mt-10 not-prose">
-            <NextImage
-              width={1536}
-              height={800}
-              src={`/images/posts/${post.slug}/${post.coverImage}`}
-              alt={`${post.title} Cover image`}
-              className="w-full rounded-md"
+    <div className="flex min-h-screen flex-col font-sans bg-neutral-200/80">
+      <Header home={home} />
+      <main className="max-w-none flex flex-1 flex-col">
+        <div className="flex-1">
+          <div className="max-w-4xl mx-auto px-4 md:px-10">
+            <Seo
+              title={post.title}
+              description={post?.summary}
+              imagePath={
+                post.coverImage
+                  ? `/images/posts/${post.slug}/${post.coverImage}`
+                  : "/images/seo-image.png"
+              }
             />
+            <div className="flex gap-3 items-center flex-wrap">
+              {authors && <AvatarList authors={authors} />}
+              <p className="font-semibold text-gray-900">{formattedNames}</p>
+            </div>
+
+            <div className="mt-4 flex justify-between">
+              <span className="flex gap-1 text-gray-700">
+                {post.publishedDate && (
+                  <p className="">
+                    {dateFormatter(post.publishedDate, "do MMM yyyy")}
+                  </p>
+                )}
+                {post.wordCount && post.wordCount !== 0 ? (
+                  <p className="">· {readTime(post.wordCount)}</p>
+                ) : null}
+              </span>
+            </div>
+
+            <div className="mt-8 prose max-w-none">
+              <h1 className="mt-4">{post.title}</h1>
+              <p className="text-lg">{post.summary}</p>
+              {post.coverImage && (
+                <div className="mt-10 not-prose">
+                  <NextImage
+                    width={1536}
+                    height={800}
+                    src={`/images/posts/${post.slug}/${post.coverImage}`}
+                    alt={`${post.title} Cover image`}
+                    className="w-full rounded-md"
+                  />
+                </div>
+              )}
+              <div className="mt-10">
+                <DocumentRenderer
+                  document={post.content}
+                  componentBlocks={{
+                    inlineCta: (props) => (
+                      <InlineCTA
+                        title={props.title}
+                        summary={props.summary}
+                        linkButton={{
+                          externalLink: props.externalLink,
+                          href: props.href,
+                          label: props.linkLabel,
+                        }}
+                      />
+                    ),
+                    divider: (props) => <Divider noIcon={props.noIcon} />,
+                    banner: (props) => (
+                      <Banner
+                        heading={props.heading}
+                        bodyText={props.bodyText}
+                        externalLink={{
+                          href: props.externalLinkHref,
+                          label: props.externalLinkLabel,
+                        }}
+                      />
+                    ),
+                    youtubeEmbed: (props) => (
+                      <YouTubeEmbed youtubeLink={props.youtubeLink} />
+                    ),
+                    tweetEmbed: (props) => <TweetEmbed tweet={props.tweet} />,
+                    loopingVideo: (props) => (
+                      <LoopingVideo src={props.src} caption={props.caption} />
+                    ),
+                    image: (props) => (
+                      <Image
+                        src={props.src}
+                        alt={props.alt}
+                        caption={props.caption}
+                      />
+                    ),
+                    testimonial: (props) => (
+                      <Testimonial
+                        quote={props.quote}
+                        author={props.author}
+                        workplaceOrSocial={props.workplaceOrSocial}
+                        socialLink={props.socialLink}
+                      />
+                    ),
+                  }}
+                />
+              </div>
+            </div>
           </div>
-        )}
-        <div className="mt-10">
-          <DocumentRenderer
-            document={post.content}
-            componentBlocks={{
-              inlineCta: (props) => (
-                <InlineCTA
-                  title={props.title}
-                  summary={props.summary}
-                  linkButton={{
-                    externalLink: props.externalLink,
-                    href: props.href,
-                    label: props.linkLabel,
-                  }}
-                />
-              ),
-              divider: (props) => <Divider noIcon={props.noIcon} />,
-              banner: (props) => (
-                <Banner
-                  heading={props.heading}
-                  bodyText={props.bodyText}
-                  externalLink={{
-                    href: props.externalLinkHref,
-                    label: props.externalLinkLabel,
-                  }}
-                />
-              ),
-              youtubeEmbed: (props) => (
-                <YouTubeEmbed youtubeLink={props.youtubeLink} />
-              ),
-              tweetEmbed: (props) => <TweetEmbed tweet={props.tweet} />,
-              loopingVideo: (props) => (
-                <LoopingVideo src={props.src} caption={props.caption} />
-              ),
-              image: (props) => (
-                <Image
-                  src={props.src}
-                  alt={props.alt}
-                  caption={props.caption}
-                />
-              ),
-              testimonial: (props) => (
-                <Testimonial
-                  quote={props.quote}
-                  author={props.author}
-                  workplaceOrSocial={props.workplaceOrSocial}
-                  socialLink={props.socialLink}
-                />
-              ),
-            }}
-          />
         </div>
-      </div>
+      </main>
+      <Footer home={home} />
     </div>
   );
 }
