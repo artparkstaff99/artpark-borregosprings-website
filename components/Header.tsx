bdcb17c30cedac061dcb7e77e0c9ba9817f48959
pkellner <peter@peterkellner.net>
2024-03-02 10:51:31 -0800

    update menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,10 @@
 import { usePathname } from "next/navigation";
+import Image from "next/image";
+import Logo from "../assets/logo.svg";
 import { cx } from "../utils/cx";
 import React from "react";
 import ToggleLanguageButton from "./toggle-language-button";
 import { useLanguage } from "./default-language-provider";
-import Link from 'next/link'; // Import Link from next/link
 
 export const baseClasses =
   "no-underline justify-center items-center focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-1 text-gray-800 px-0 hover:text-cyan-700 hover:bg-none bg-none font-medium shrink-0";
@@ -37,16 +38,26 @@ const Header = ({ home }: { home: any }) => {
   const MobileMenu = () => {
     return (
       <div className="mx-4 -mt-1 p-4 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5 z-10 max-w-md float-right">
+        {/* Nav items */}
         <div className="grid grid-flow-row not-prose">
           {NavItems.map((item) => (
-            <Link key={item.slug} href={item.slug} passHref>
-              <p className={cx(
-                pathname === item.slug ? "text-text-cyan-700" : "",
-                "pb-5 first:pt-3 px-3 last:pb-3 pt-5 border-b-2 border-slate-100 border-spacing-2 last:border-b-0 no-underline my-0 pb-2 font-bold"
-              )}>
+            <a
+              key={item.slug}
+              className={
+                "pb-5 first:pt-3 px-3 last:pb-3 pt-5 border-b-2 border-slate-100 border-spacing-2 last:border-b-0 no-underline"
+              }
+              href={item.slug}
+            >
+              <p
+                className={cx(
+                  pathname === item.slug ? "text-text-cyan-700" : "",
+                  "my-0 pb-2 font-bold"
+                )}
+              >
                 {item.name}
               </p>
-            </Link>
+              <p className="my-0">{item.description}</p>
+            </a>
           ))}
         </div>
       </div>
@@ -59,11 +70,20 @@ const Header = ({ home }: { home: any }) => {
         <KeystaticBanner message={language === "en" ? home.special_banner_top_en : home.special_banner_top_es} />
       )}
       <div className="flex justify-between items-center px-4 md:px-28 py-4 md:py-10 h-full">
-        <Link href="/" >
+        <a
+          className="no-underline text-xl font-bold"
+          href="/"
+          aria-label="Link to home page"
+        >
           <p className="no-underline text-xl font-bold" aria-label="Link to home page">
-            🌎 &nbsp; {language === "en" ? home.top_left_header_en : home.top_left_header_es}
-          </p>
-        </Link>
+          🌎 &nbsp; {language === "en" ? home.top_left_header_en : home.top_left_header_es}
+        </p>
+        </a>
+
+        <ToggleLanguageButton />
+
+
+        {/* Mobile Hamburger Icon button */}
         <nav className="-mr-2 min-[768px]:hidden">
           <button
             type="button"
@@ -71,27 +91,61 @@ const Header = ({ home }: { home: any }) => {
             aria-label="Open menu"
             onClick={() => setMenuOpen(!menuOpen)}
           >
-            {/* SVG icons */}
+            {menuOpen ? (
+              <svg
+                className="h-6 w-6 stroke-gray-900"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                ></path>
+              </svg>
+            ) : (
+              <svg
+                className="h-6 w-6 stroke-gray-900"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                aria-hidden="true"
+                focusable="false"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                ></path>
+              </svg>
+            )}
           </button>
         </nav>
-        <nav className="items-center space-x-8 hidden min-[768px]:flex text-2xl">
+        {/* Desktop nav */}
+        <nav className="items-center space-x-8 hidden min-[768px]:flex">
           {NavItems.map((item) => (
-            <Link key={item.slug} href={item.slug} >
-              <p className={cx(
+            <a
+              key={item.slug}
+              className={cx(
                 baseClasses,
-                pathname === item.slug ? "border-text-cyan-700 border-b-2" : "border-transparent",
-                "cursor-pointer"
-              )}>
-                {item.name}
-              </p>
-            </Link>
+                pathname === item.slug
+                  ? "border-text-cyan-700 border-b-2"
+                  : "border-transparent"
+              )}
+              href={item.slug}
+            >
+              {item.name}
+            </a>
           ))}
         </nav>
-        <div className="flex items-center space-x-4">
-          <ToggleLanguageButton />
-        </div>
       </div>
-      {menuOpen && <MobileMenu />}
+      {menuOpen ? <MobileMenu /> : null}
     </header>
   );
 };
