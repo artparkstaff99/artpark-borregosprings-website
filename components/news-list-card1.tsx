fed765905c0aa9d1b9beaa7da4451eb5b7048ad6
pkellner <peter@peterkellner.net>
2024-03-08 16:26:12 -0800

    go

--- components/news-list-card1.tsx
+++ components/news-list-card1.tsx
@@ -1,45 +1,5 @@
-function formatDate(dateString: string) {
-  const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  const date = new Date(dateString);
-  const year = date.getFullYear();
-  const month = months[date.getMonth()];
-  const day = date.getDate();
+import formatDate from "../utils/format-date";
 
-  // Adding suffix to day
-  let daySuffix;
-  switch (day) {
-    case 1:
-    case 21:
-    case 31:
-      daySuffix = "st";
-      break;
-    case 2:
-    case 22:
-      daySuffix = "nd";
-      break;
-    case 3:
-    case 23:
-      daySuffix = "rd";
-      break;
-    default:
-      daySuffix = "th";
-  }
-
-  return `${month} ${day}${daySuffix}, ${year}`;
-}
 
 export default function ArticleCard({
   slug,
